29ed4b71ca9c364691ee2b44852d9bc74cd3756b
Evan Bacon <baconbrix@gmail.com>
2019-08-15 14:13:27 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60144109bbe17651b1eb050c29e4f6f4.js"
+  "/precache-manifest.bb131bfc4fd351173f9adb935208ffc1.js"
 );
 
 workbox.clientsClaim();
